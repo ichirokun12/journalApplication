954dfd030610a50f0b9b0324ba4870d2614fba70
ichirokun12 <rajat.k.singh1209@gmail.com>
2025-03-24 22:39:37 +0530

    hiding api

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,8 +11,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    public static final String apiKey = "59247bf67d64e793ebcc4d6812f5fd7d";
-    public static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+    @Value("${weather.api.key}")
+    private String apiKey;
+    private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
     private RestTemplate restTemplate;
